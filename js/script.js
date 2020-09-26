c5ad39eeed58023d50fe68fc4399378746e1d36d
Valeriia <oksiutenkovaleriia@gmail.com>
2020-09-26 22:08:29 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const answers = [];
+answers[0] = prompt('Как Ваше имя', '');
+answers[1] = prompt('Как Ваша фамилия', '');
+answers[2] = prompt('Сколько Вам лет', '');
+document.write(answers);
