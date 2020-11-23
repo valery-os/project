f485d61469b1335a3981b2e406660140edf2abff
Valeriia <oksiutenkovaleriia@gmail.com>
2020-11-23 02:37:50 +0200

    practice part#2

--- js/script.js
+++ js/script.js
@@ -1,18 +1,27 @@
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 const personaMoviaDB = {
-    count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
     privar: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
+// const a = prompt('Один из последних просмотренных фильмов?', ''),
+//       b = prompt('На сколько оцените его?', ''),
+//       c = prompt('Один из последних просмотренных фильмов?', ''),
+//       d = prompt('На сколько оцените его?', '');
 
-personaMoviaDB.movies[a] = b;
-personaMoviaDB.movies[c] = d;
+// personaMoviaDB.movies[a] = b;
+// personaMoviaDB.movies[c] = d;
+
+// console.log(personaMoviaDB);
+
+
+const questions = ['Один из последних просмотренных фильмов?', 'На сколько оцените его?'];
+let repeat = 2;
+
+for (let i = 0; i < repeat; i++) {    
+    personaMoviaDB.movies[prompt(questions[0], '')] = prompt(questions[1], '');    
+}
 
 console.log(personaMoviaDB);
